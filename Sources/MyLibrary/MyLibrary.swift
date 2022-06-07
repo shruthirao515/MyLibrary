d4f59431ef6eccd2d63d0405d0243d7254d44ab9
shruthirao515 <shruthirao515@gmail.com>
2022-06-07 02:18:50 -0700

    First commit

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -4,3 +4,18 @@ public struct MyLibrary {
     public init() {
     }
 }
+public struct Student {
+    
+    var name : String
+    var id : Int
+    var marks : Int
+    
+    public init (name: String, roll: Int, marks: Int)  {
+        self.name = name
+        self.id = roll
+        self.marks = marks
+    }
+    public func getPercentage() -> Double {
+        return Double(marks) / 100.0
+    }
+}
--- Tests/MyLibraryTests/MyLibraryTests.swift
+++ Tests/MyLibraryTests/MyLibraryTests.swift
@@ -7,5 +7,8 @@ final class MyLibraryTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         XCTAssertEqual(MyLibrary().text, "Hello, World!")
+        let std = Student(name: "John", roll:1, marks:100)
+        std.getPercentage()
+        
     }
 }
